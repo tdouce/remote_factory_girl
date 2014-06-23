f61472767d622b7ed2b5844478b16ccb0e23d2d3
tdouce <travisdouce@gmail.com>
2014-06-23 09:53:55 -0400

    Adds tests for Config#has_home?

--- spec/models/remote_factory_girl/config_spec.rb
+++ spec/models/remote_factory_girl/config_spec.rb
@@ -5,6 +5,8 @@ require 'remote_factory_girl/hash_to_dot'
 
 describe RemoteFactoryGirl::Config do
 
+  let(:config) { RemoteFactoryGirl::Config.new }
+
   describe 'initialize' do
 
     describe 'default configuration' do
@@ -22,8 +24,6 @@ describe RemoteFactoryGirl::Config do
 
   describe '#home_url' do
 
-    let(:config) { RemoteFactoryGirl::Config.new }
-
     it 'should return a url with port if port is configured' do
       config.home[:host] = 'localhost'
       config.home[:port] = 5555
@@ -34,6 +34,24 @@ describe RemoteFactoryGirl::Config do
       config.home[:host] = 'localhost_no_port'
       config.home[:port] = nil 
       expect(config.home_url).to eq('http://localhost_no_port/remote_factory_girl/home')
+    end
+  end
+
+  describe '#has_home?' do
+    it 'should return false when host and end_point are not set' do
+      config.home[:host]      = nil 
+      config.home[:end_point] = nil 
+      expect(config.has_home?).to be_false
+    end
+
+    it 'should return false when host is not set and end_point is set' do
+      expect(config.has_home?).to be_false
+    end
+
+    it 'should return true when host and end_point is set' do
+      config.home[:host]      = 'localhost'
+      config.home[:end_point] = 'some_where'
+      expect(config.has_home?).to be_true
     end
   end
 end
